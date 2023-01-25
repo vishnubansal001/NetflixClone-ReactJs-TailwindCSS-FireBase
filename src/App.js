a27186074c37a708a9b9e5ecdf8408c7cc6d6c0b
vishnubansal001 <vishnu1549.be21@chitkara.edu.in>
2023-01-26 01:45:04 +0530

    Added all the main files and folders

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Navbar from './components/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import { AuthContextProvider } from './context/AuthContext';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Account from './pages/Account';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <AuthContextProvider>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Home/>}></Route>
+          <Route path='/login' element={<Login/>}></Route>
+          <Route path='/signup' element={<Signup/>}></Route>
+          <Route path='/account' element={<ProtectedRoute><Account/></ProtectedRoute>}></Route>
+        </Routes>
+      </AuthContextProvider>
+    </>
   );
 }
 
---
+++ src/components/ProtectedRoute.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import { UserAuth } from '../context/AuthContext';
+const ProtectedRoute = ({children}) => {
+  const {user} = UserAuth()
+  if(!user){
+    return <Navigate to='/'></Navigate>
+  }else{
+    return children;
+  }
+}
+
+export default ProtectedRoute
